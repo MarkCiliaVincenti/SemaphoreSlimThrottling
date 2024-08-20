be698f2e16471f973ac6680d224265807b59b552
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2024-08-20 15:50:23 +0200

    Improved coverage.

--- SemaphoreSlimThrottling.Tests/Tests.cs
+++ SemaphoreSlimThrottling.Tests/Tests.cs
@@ -6,7 +6,7 @@ namespace SemaphoreSlimThrottling.Tests
     public class Tests
     {
         [Fact]
-        public async Task TestSemaphoreSlimThrottle()
+        public async Task TestSemaphoreSlimThrottleWithMaxCount()
         {
             var semaphoreSlimThrottle = new SemaphoreSlimThrottle(-2, 1);
             Assert.Equal(-2, semaphoreSlimThrottle.CurrentCount);
@@ -27,6 +27,33 @@ namespace SemaphoreSlimThrottling.Tests
             await semaphoreSlimThrottle.WaitAsync();
             Assert.Equal(0, semaphoreSlimThrottle.CurrentCount);
             Assert.Equal(0, semaphoreSlimThrottle.Release());
+        }
+
+        [Fact]
+        public async Task TestSemaphoreSlimThrottleWithoutMaxCount()
+        {
+            var semaphoreSlimThrottle = new SemaphoreSlimThrottle(-2);
+            Assert.Equal(-2, semaphoreSlimThrottle.CurrentCount);
+            Assert.Equal(-2, semaphoreSlimThrottle.Release());
+            Assert.Equal(-1, semaphoreSlimThrottle.CurrentCount);
+            Assert.Equal(-1, semaphoreSlimThrottle.Release());
+            Assert.Equal(0, semaphoreSlimThrottle.CurrentCount);
+            Assert.Equal(0, semaphoreSlimThrottle.Release());
+            Assert.Equal(1, semaphoreSlimThrottle.CurrentCount);
+            await semaphoreSlimThrottle.WaitAsync();
+            Assert.Equal(0, semaphoreSlimThrottle.CurrentCount);
+            Assert.Equal(0, semaphoreSlimThrottle.Release());
+
+            semaphoreSlimThrottle = new SemaphoreSlimThrottle(-2);
+            Assert.Equal(-2, semaphoreSlimThrottle.CurrentCount);
+            Assert.Equal(-2, semaphoreSlimThrottle.Release(3));
+            Assert.Equal(1, semaphoreSlimThrottle.CurrentCount);
+            await semaphoreSlimThrottle.WaitAsync();
+            Assert.Equal(0, semaphoreSlimThrottle.CurrentCount);
+            Assert.Equal(0, semaphoreSlimThrottle.Release());
+
+            semaphoreSlimThrottle = new SemaphoreSlimThrottle(1);
+            semaphoreSlimThrottle.Dispose();
         }
     }
 }
